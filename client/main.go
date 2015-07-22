b1696c187d5310f2c51e0fd45f861651b7e9506f
Richo Healey <richo@psych0tik.net>
2015-07-22 14:10:15 -0700

    Enough plumbing to stand up a fuzzer

--- client/main.go
+++ client/main.go
@@ -6,10 +6,64 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
 type Server struct {
 	hostport string
+}
+
+type Fuzzer struct {
+}
+
+func (f *Fuzzer) run() {
+	// HACK
+	os.Mkdir("input", 0755)
+	fh, _ := os.OpenFile("input/hi", os.O_WRONLY|os.O_CREATE, 0755)
+	fh.Write([]byte("hi"))
+	fh.Close()
+	cmd := exec.Command(f.path(),
+		"-o", "output",
+		"-i", "input",
+		"./target",
+	)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatalf("Couldn't get stdout handle", err)
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatalf("Couldn't get stderr handle", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("Couldn't start fuzzer", err)
+	}
+
+	go func() {
+		io.Copy(os.Stdout, stdout)
+	}()
+
+	go func() {
+		io.Copy(os.Stderr, stderr)
+	}()
+
+	log.Printf("Waiting for fuzzer to exit")
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func (f *Fuzzer) path() string {
+	root := os.Getenv("AFL")
+	if root == "" { // Not found, hopefully it's in PATH
+		return "afl-fuzz"
+	}
+	return fmt.Sprintf("%s/afl-fuzz", root)
 }
 
 func (s *Server) fetchToFile(resource, file string) {
@@ -66,4 +120,9 @@ func main() {
 
 	server := Server{args[1]}
 	server.FetchTarget()
+
+	fuzzer := Fuzzer{}
+	fuzzer.run()
+
+	// TODO(richo) Fetch the input corpus
 }
