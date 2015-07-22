fbd6381abd2cb59e31f9cf06b2f31c96cb1b174a
Richo Healey <richo@psych0tik.net>
2015-07-22 14:51:27 -0700

    Serve up the inputs to clients

--- server/main.go
+++ server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+
 	"net/http"
 
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
+
+	"github.com/richo/roving/types"
 )
 
 // For now, the server stores a great deal of state in memory, although it will
@@ -34,10 +39,41 @@ func target(c web.C, w http.ResponseWriter, r *http.Request) {
 	w.Write(binary)
 }
 
+func inputs(c web.C, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	corpus := types.InputCorpus{}
+
+	files, err := ioutil.ReadDir("input")
+	if err != nil {
+		log.Fatalf("Couldn't open input directory", err)
+	}
+
+	for _, f := range files {
+		path := fmt.Sprintf("input/%s", f.Name())
+		var buf []byte
+
+		buf, err = ioutil.ReadFile(path)
+		if err != nil {
+			log.Fatalf("Couldn't read %s", path, err)
+		}
+
+		inp := types.Input{
+			Name: f.Name(),
+			Body: base64.StdEncoding.EncodeToString(buf),
+		}
+		corpus.Add(inp)
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(corpus)
+}
+
 func setupAndServe() {
 	goji.Put("/state", put)
 	goji.Get("/state", get)
 	goji.Get("/target", target)
+	goji.Get("/inputs", inputs)
 	goji.Serve()
 }
 
