e39ca8e79c957332db508ff376ae424bbc8e4008
Richo Healey <richo@psych0tik.net>
2016-03-10 00:24:53 -0800

    Bail gracefully if templates can't be loaded

--- server/app.go
+++ server/app.go
@@ -9,16 +9,21 @@ import (
 )
 
 var indexTemplate *template.Template
+var webfaceEnabled bool = true
 
 func init() {
 	var err error
 	indexTemplate, err = template.New("index.html").ParseFiles("server/templates/index.html")
 	if err != nil {
-		log.Fatalf("Couldn't parse template")
+		log.Printf("WARN: Couldn't parse templates, disabling web interface:", err)
 	}
 }
 
 func index(c web.C, w http.ResponseWriter, r *http.Request) {
+	if !webfaceEnabled {
+		w.Write([]byte("Couldn't initialize templates. Web interface disabled"))
+		return
+	}
 	nodesLock.RLock()
 	defer nodesLock.RUnlock()
 	err := indexTemplate.Execute(w, nodes)
